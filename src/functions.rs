d2ef390a4c79090e98769b81fa64c62b1a615db0
Xiaonuo Gantan <xgantan@execvision.io>
2023-09-28 07:38:52 -0400

    Add 2 more demos

--- src/functions.rs
+++ src/functions.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering::Less;
+use std::f32::NAN;
+use std::io::Read;
 use std::thread;
 
 use console::style;
@@ -88,6 +91,90 @@ pub const input_parsing_fixed: Function = Function {
         } else {
             println!("It is not {}!", style("2").bold().red());
         }
+
+        Ok(())
+    },
+};
+
+pub const ord_int: Function = Function {
+    name: "Sort int",
+    execute: || -> std::io::Result<()> {
+        let stdout = Term::stdout();
+        stdout.write_line("\
+            What's the output of the following program?\
+            \n\
+            \nfn main() {\
+            \n    let mut numbers = vec![1, 2, 3, 4, 5];\
+            \n    numbers.sort();\
+            \n    println!(\"{:?}\", numbers);\
+            \n}\n
+            ")?;
+        std::io::stdin().read(&mut [0u8]).expect("Unable to read stdin");
+        let mut numbers = vec![1, 2, 3, 4, 5];
+        numbers.sort();
+        println!("{:?}", numbers);
+
+        Ok(())
+    },
+};
+
+pub const ord_float: Function = Function {
+    name: "Sort float",
+    execute: || -> std::io::Result<()> {
+        let stdout = Term::stdout();
+        stdout.write_line("\
+            What's the output of the following program?\
+            \n\
+            \nfn main() {\
+            \n    let mut numbers = vec![1.0, 2.0, 3.0, NAN, 5.0];\
+            \n    numbers.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Less));\
+            \n    println!(\"{:?}\", numbers);\
+            \n}\n
+            ")?;
+        std::io::stdin().read(&mut [0u8]).expect("Unable to read stdin");
+        let mut numbers = vec![1.0, 2.0, 3.0, f32::NAN, 5.0];
+        numbers.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Less));
+        println!("{:?}", numbers);
+
+        Ok(())
+    },
+};
+
+pub const stackoverflow: Function = Function {
+    name: "Stack Overflow",
+    execute: || -> std::io::Result<()> {
+        let stdout = Term::stdout();
+        stdout.write_line("\
+            What's the output of the following program?\
+            \n\
+            \nfn main() {\
+            \n    let c = Box::new([0f64; 100_000_000]);\
+            \n    println!(\"{}\", c.len());\
+            \n}\n
+            ")?;
+        std::io::stdin().read(&mut [0u8]).expect("Unable to read stdin");
+        let c = Box::new([0f64; 100_000_000]);
+        println!("{}", c.len());
+
+        Ok(())
+    },
+};
+
+pub const stackoverflow_fixed: Function = Function {
+    name: "Stack Overflow Fixed",
+    execute: || -> std::io::Result<()> {
+        let stdout = Term::stdout();
+        stdout.write_line("\
+            What's the output of the following program?\
+            \n\
+            \nfn main() {\
+            \n    let c = vec![0f64; 100_000_000];\
+            \n    println!(\"{}\", c.len());\
+            \n}\n
+            ")?;
+        std::io::stdin().read(&mut [0u8]).expect("Unable to read stdin");
+        let c = vec![0f64; 100_000_000];
+        println!("{}", c.len());
 
         Ok(())
     },
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use dialoguer::{
     theme::ColorfulTheme,
 };
 use console::{style, Term};
-use crate::functions::{input_parsing, input_parsing_explained, input_parsing_fixed};
+use crate::functions::{input_parsing, input_parsing_explained, input_parsing_fixed, ord_float, ord_int, stackoverflow, stackoverflow_fixed};
 
 struct Quirk<'a> {
     name: &'a str,
@@ -18,6 +18,14 @@ fn main() {
         Quirk {
             name: "Input End-of-line Parsing",
             func: input_string_parsing,
+        },
+        Quirk {
+            name: "Sort",
+            func: ord_trait_numbers,
+        },
+        Quirk {
+            name: "Stackoverflow",
+            func: stackoverflow_demo,
         },
     ];
     loop {
@@ -37,6 +45,20 @@ fn main() {
 
 fn input_string_parsing() -> std::io::Result<()> {
     let items = vec![input_parsing, input_parsing_explained, input_parsing_fixed];
+    select(items)
+}
+
+fn ord_trait_numbers() -> std::io::Result<()> {
+    let items = vec![ord_int, ord_float];
+    select(items)
+}
+
+fn stackoverflow_demo() -> std::io::Result<()> {
+    let items = vec![stackoverflow, stackoverflow_fixed];
+    select(items)
+}
+
+fn select(items: Vec<Function>) -> std::io::Result<()> {
     let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Select an item to explore")
         .default(0)
